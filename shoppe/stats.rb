e2395640343a2cd5240054f7c13a68936959467f
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-05-28 17:31:10 -0400

    refactored beautifully

--- shoppe/stats.rb
+++ shoppe/stats.rb
@@ -10,17 +10,18 @@ def file_path file_name
   File.expand_path "../data/#{file_name}.json", __FILE__
 end
 
-t = TransactionParser.new file_path("transactions")
-d = DataParser.new file_path("data")
+transactions = TransactionParser.new file_path("transactions")
+data = DataParser.new file_path("data")
 
-d.parse!
-t.parse!
+data.parse!
+transactions.parse!
 
-def find_frequent_buyer
+def find_frequent_buyer transactions, data
+
   transaction_by_user = {}
   transaction_by_user.default = 0
 
-  (t.transactions).each do |x|
+  (transactions.transactions).each do |x|
 
     transaction_by_user[x.user_id] += 1
   end
@@ -29,28 +30,28 @@ def find_frequent_buyer
 
   buyername = nil
 
-  (d.users).each do |x|
+  (data.users).each do |x|
     if x.id == 16
       buyername = x.name
     end
   end
 
-  puts "The user that made the most orders was #{buyername}."
+  puts "The customer that made the most orders was #{buyername}."
 end
 
-def ergo_lamps_sold
+def ergo_lamps_sold data, transactions
 
   lamps = 0
   itemid = nil
 
-  d.items.each do |item|
+  data.items.each do |item|
 
     if item.name == "Ergonomic Rubber Lamp"
       itemid = item.id
     end
   end
 
-  t.transactions.each do |x|
+  transactions.transactions.each do |x|
     if x.item_id == itemid
       lamps += x.quantity
     end
@@ -58,25 +59,24 @@ def ergo_lamps_sold
   puts "We sold #{lamps} Ergonomic Rubber Lamps"
 end
 
-def sold_from_Tools
+def sold_from_tools transactions, data
   tools = 0
-  t.transactions.each do |x|
-    d.items.each do |y|
+  transactions.transactions.each do |x|
+    data.items.each do |y|
       if (y.id == x.item_id) && (y.category.include?"Tools")
         tools += x.quantity
       end
     end
   end
 
-  puts "We sold #{tools.count} items from the Tools category."
+  puts "We sold #{tools} items from the Tools category."
 end
 
-
-def total_revenue
+def total_revenue transactions, data
   revenue = 0
 
-  t.transactions.each do |x|
-    d.items.each do |y|
+  transactions.transactions.each do |x|
+    data.items.each do |y|
       if x.item_id == y.id
         revenue += y.price * x.quantity
       end
@@ -87,49 +87,48 @@ def total_revenue
   puts "Total revenue is: $#{revenue.round(2)}."
 end
 
-# * Harder: the highest grossing category was __
+def highest_grossing_category transactions, data
 
-# list of categories
-# for each transaction,
-  #look at the category,
-  #for each category, if it is contained in t.category, add price * quantity to it
+  categories = []
+  parsed_categories_revenue = {}
+  revenue_by_category = {}
+  revenue_by_category.default = 0
 
-categories = []
-parsed_categories_revenue = {}
-revenue_by_category = {}
-revenue_by_category.default = 0
-
-
-d.items.each do |x|
-  categories.push(x.category)
-end
-
-categories.each do |x|
-  if x.include? " & "
-    x.delete! " & "
+  # Get list of individual categories
+  data.items.each do |x|
+    categories.push(x.category)
   end
-end
 
-categories = categories.join.split /(?=[A-Z])/
-categories.each do |x|
-  parsed_categories_revenue[x] = 0
-end
+  categories.each do |x|
+    if x.include? " & "
+      x.delete! " & "
+    end
+  end
 
+  categories = categories.join.split /(?=[A-Z])/
+  categories.each do |x|
+    parsed_categories_revenue[x] = 0
+  end
 
-  t.transactions.each do |x|
+  # Get revenue by category
+  transactions.transactions.each do |x|
     parsed_categories_revenue.each do |key,value|
-    d.items.each do |y|
-      if (x.item_id == y.id) && (y.category.include? key)
-        parsed_categories_revenue[key] += (y.price * x.quantity)
+      data.items.each do |y|
+        if (x.item_id == y.id) && (y.category.include? key)
+          parsed_categories_revenue[key] += (y.price * x.quantity)
+        end
       end
-
-    end
-
     end
   end
-
+  # Find category with max revenue
   highest_cat = parsed_categories_revenue.key(parsed_categories_revenue.values.max)
 
-puts "The highest grossing category is #{highest_cat}."
+  puts "The highest grossing category is #{highest_cat}."
 
-# binding.pry
+end
+
+question1 = find_frequent_buyer transactions, data
+question2 = ergo_lamps_sold data, transactions
+question3 = sold_from_tools transactions, data
+question4 = total_revenue transactions, data
+question5 = highest_grossing_category transactions, data
